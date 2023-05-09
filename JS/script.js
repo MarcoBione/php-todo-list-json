987d1328f7e909aa60909584887acf390d0ac3a2
MarcoBione <marcobione.work@gmail.com>
2023-05-09 19:21:46 +0200

    some mods but does not working

--- JS/script.js
+++ JS/script.js
@@ -1,17 +1,37 @@
-import { createApp } from 'vue';
+const { createApp } = Vue;
 
 createApp({
+
     data() {
         return {
-            apiUrl: 'script.php',
+            apiUrl: './script.php',
             tasksList: [],
+            todoItem: '',
         }
     },
+
     methods: {
+
         tasksListReader() {
             axios.get(this.apiUrl).then((res) => {
                 this.tasksList = res.data;
             });
         },
-    }
+        uploadTask() {
+
+            console.log(this.todoItem);
+
+            const data = {
+                todoItem: this.todoItem,
+            };
+
+            axios.post(this.apiUrl, data, { headers: { 'Content-Type': 'multipart/form-data' } }).then((res) => {
+                this.todoItem = '';
+            });
+        },
+
+    },
+    mounted() {
+        this.tasksListReader();
+    },
 }).mount('app');
--- index.php
+++ index.php
@@ -33,40 +33,38 @@ Abilitare l’eliminazione di un task
     <title>To Do List</title>
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-KK94CHFLLe+nY2dmCWGMq91rCGa5gtU4mk92HdvYe+M/SXH301p5ILy+dN9+nJOZ" crossorigin="anonymous">
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.4.0/css/all.min.css" integrity="sha512-iecdLmaskl7CVkqkXNQ/ZH/XLlvWZOJyj7Yy7tcenmpD1ypASozpmT/E0iPtmFIB46ZmdtAc9eNBvH0H/ZpiBw==" crossorigin="anonymous" referrerpolicy="no-referrer" />
+    <script src="https://unpkg.com/vue@3/dist/vue.global.js"></script>
+    <script src="https://unpkg.com/axios/dist/axios.min.js"></script>
+    <script src="./JS/script.js"></script>
 </head>
 <body>
-<div id="app">
+    <div id="app">
 
-    <header class="border-bottom border-primary">
-        <div class="container d-flex align-items-center justify-content-between">
-            <div class="title">
-                <h1 class="text-primary">To Do List</h1>
-            </div>
-            <div>
-                <div class="input-group">
-                    <input type="text" class="form-control shadow-none border-bottom border-primary" name="task" placeholder="Inserisci una task...">
-                    <button class="btn btn-outline-primary" @click="tasksListReader"><i class="fa-solid fa-plus"></i></button>
+        <header class="border-bottom border-primary">
+            <div class="container d-flex align-items-center justify-content-between">
+                <div class="title">
+                    <h1 class="text-primary">To Do List</h1>
+                </div>
+                <div>
+                    <div class="input-group">
+                        <input type="text" class="form-control shadow-none border-bottom border-primary" v-model="todoItem" @keyup.enter="uploadTask" placeholder="Inserisci una task...">
+                        <button class="btn btn-outline-primary" @click="uploadTask"><i class="fa-solid fa-plus"></i></button>
+                    </div>
                 </div>
             </div>
-        </div>
-    </header>
+        </header>
 
-    <main>
-        <div class="container py-5">
-            <ul class="list-group" v-for="tasks,index in tasksList">
-                <li class="list-group-item border-primary text-primary">{{tasks}}</li>
-            </ul>
-        </div>
-    </main>
+        <main>
+            <div class="container py-5">
+                <ul class="list-group">
+                    <li class="list-group-item border-primary text-primary" v-for="(tasks,index) in tasksList" :key="index">{{tasks.name}}</li>
+                </ul>
+            </div>
+        </main>
 
-    <footer>
+        <footer>
 
-    </footer>
-</div>
-
-
-<script src="https://unpkg.com/vue@3/dist/vue.global.js"></script>
-<script src="https://unpkg.com/axios/dist/axios.min.js"></script>
-<script src="./JS/script.js"></script>
+        </footer>
+    </div>
 </body>
 </html>
